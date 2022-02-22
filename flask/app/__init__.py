bfe706639fe8cbc86b209951a847084294b8492d
Gösta <gosta@MacBook-Pro-som-tillhor-Gosta.local>
2022-02-23 00:00:45 +0100

    working on requests from node red

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, Response
 from app.dynamo_access import get_all_readings
+import json
 
 app = Flask(__name__)
 
@@ -14,6 +15,13 @@ def index():
 @app.post('/api/v1.0/alarm')
 def incoming_alarm():
     data = request.json
+    print(type(data))
     alarm_activated.append(data)
+    print(type(alarm_activated))
     return Response('"Status":"Succeeded"', 200, content_type="application/json")
 
+
+@app.get('/api/v1.0/alarm')
+def get_alarm_client():
+    print("Jag har lyckats skicka post request")
+    return Response(json.dumps(alarm_activated), 200, content_type='application/json')
