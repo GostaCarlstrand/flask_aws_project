c3abb91304523d8f1ab73f7d718654f7254d376e
Gösta <gosta@MacBook-Pro-som-tillhor-Gosta.local>
2022-02-27 13:55:49 +0100

    Activate alarm button is almost done

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -5,11 +5,35 @@ import json
 app = Flask(__name__)
 
 alarm_activated = []
+alarm_state = False
+
+
+# Not always correct value on alarm_state
+#
+#
+#
+#
+
+
+@app.post('/alarm_activate')
+def activate_alarm():
+    activate_msg = '{"Alarm":"Activated"}'
+    deactivate_msg = '{"Alarm":"Deactivated"}'
+    global alarm_state
+    if not alarm_state:
+        alarm_state = True
+        print("sant")
+        return Response(activate_msg, 200, content_type="application/json")
+    if alarm_state:
+        alarm_state = False
+        print("falskt")
+        return Response(deactivate_msg, 200, content_type="application/json")
+
 
 @app.get('/')
 def index():
     readings = get_all_readings()
-    return render_template('index.html', readings=readings)
+    return render_template('index.html', readings=readings, alarm=alarm_state)
 
 
 @app.post('/api/v1.0/alarm')
@@ -23,5 +47,4 @@ def incoming_alarm():
 
 @app.get('/api/v1.0/alarm')
 def get_alarm_client():
-    print("Jag har lyckats skicka post request")
     return Response(json.dumps(alarm_activated), 200, content_type='application/json')
